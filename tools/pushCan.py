1dbc2bb40d27a7455ff808444abc2bafc81aeaa7
kenygaar <kenygaar@mines.edu>
2015-03-27 11:58:02 -0600

    made it so pushCan pushes values to the can bus in parallel and cleans up properly when it is done

--- tools/pushCan.py
+++ tools/pushCan.py
@@ -3,6 +3,11 @@ from can.interfaces.interface import Bus
 import struct
 import sys
 import re
+from multiprocessing import Process, Queue
+
+# command to send all values
+# rpm 31000:27000 tps 1:3000 fuel_open_time 1:3000 ignition_angle 1:3000 barometer 1:3000 map 1:3000 lambda 1:3000 pressure_type 1:3000 analog1 1:3000 analog2 1:3000 analog3 1:3000 analog4 1:3000 analog5 1:3000 analog6 1:3000 analog7 1:3000 analog8 1:3000 frequency1 1:3000 frequency2 1:3000 frequency3 1:3000 frequency4 1:3000 battery_voltage 1:3000 air_temp 1:3000 coolant_temp 1:3000 temp_type 1:3000 analog5_thermistor 1:3000 analog7_thermistor 1:3000 version_major 1:3000 version_minor 1:3000 version_build 1:3000 tbd 1:3000 
+
 
 param2id = {
 	'rpm': 218099784,
@@ -46,31 +51,47 @@ def pushCan(canId, data):
 	msg = can.Message(arbitration_id=canId, data=(data).to_bytes(4, byteorder='little'), extended_id=True)
 	bus.send(msg);
 
+def canWriter(canId, value):
+	# spawned sub process for writing to the can bus
+	if(range_pattern.match(value) != None):
+		match_object= range_pattern.match(value)
+		left = int(match_object.group(1))
+		right = int(match_object.group(2))
+		if(left < right):
+			for x in range(left, right + 1):
+				pushCan(canId, x)
+		elif(left > right):
+			for x in range(0, left - right + 1):
+				pushCan(canId, left - x)
+		else:
+			pushCan(canId, left)
+	else:
+		pushCan(canId, int(value))
+	return True
+
 while True:
+	processes = []
+	results = Queue()
+
 	command = input("")
 	commands = command.split(" ")
 	if((len(sys.argv) - 1) % 2 == 0):
+
+		# pull all the commands from the user input
 		for x in range(0,len(commands),2):
 			parameter = commands[x]
+
 			if(parameter in param2id):
 				canId = param2id[parameter]
-
 				value = commands[x+1]
-				if(range_pattern.match(value) != None):
-					match_object= range_pattern.match(value)
-					left = int(match_object.group(1))
-					right = int(match_object.group(2))
-					if(left < right):
-						for x in range(left, right + 1):
-							pushCan(canId, x)
-					elif(left > right):
-						for x in range(0, left - right + 1):
-							pushCan(canId, left - x)
-					else:
-						pushCan(canId, left)
-				else:
-					pushCan(canId, int(value))
-
+				# spawn sub process
+				p = Process(target=canWriter, args=(canId, value))
+				p.start()
+				processes.append(p)
 	else:
 		print("not enough pairs")
-
+	
+	numberOfProcesses = len(processes)
+	for p in processes:
+		p.join()
+		p.terminate()
